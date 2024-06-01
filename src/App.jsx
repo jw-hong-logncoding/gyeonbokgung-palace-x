d646fcb029a5807a9c76535b111da3802de55b7a
Daewoong Ko <kodw4284@gmail.com>
2024-06-02 00:35:28 +0900

    feat: implement Map page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NotFound from './pages/NotFound'
 import Home from './pages/Home'
 import NavBarLayout from './components/Layout/NavBarLayout/NavBarLayout'
 import Community from './components/Community'
+import Map from './components/Map'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<NavBarLayout />}>
           <Route index path="/" element={<Home />} />
           <Route index path="/community" element={<Community />} />
+          <Route index path="/map" element={<Map />} />
         </Route>
         <Route path="*" element={<NotFound />}/>
       </Routes>
