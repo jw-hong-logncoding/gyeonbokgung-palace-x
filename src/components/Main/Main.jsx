5563f8b8685154ce262bf63467a97434b0a6ea00
ckim48 <ckim48@u.rochester.edu>
2024-08-07 12:37:03 +0900

    gg

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography, Button } from "@mui/material";
 import { keyframes } from '@mui/system';
+import { useNavigate } from "react-router-dom";
 import IMAGES from "../../assets/images";
 import * as S from "./Main.style";
 import LoginButton from "../LoginButton";
@@ -19,9 +20,25 @@ const fadeIn = keyframes`
   }
 `;
 
+// Function to generate a random string of specified length
+const generateRandomString = (length) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
 const Main = () => {
     const { userData } = useUserData();
     const { handleGoogleLogin } = useGoogleLogin();
+    const navigate = useNavigate(); // Use the navigate hook from react-router-dom
+
+    const handleLearnMoreClick = () => {
+        const randomString = generateRandomString(5);
+        navigate(`/?page=about${randomString}`);
+    };
 
     return (
         <Box>
@@ -125,7 +142,7 @@ const Main = () => {
                     sx={{
                         display: 'flex',
                         justifyContent: 'center',
-                        animation: `${fadeIn} 2s ease-in-out` // Apply the animation
+                        animation: `${fadeIn} 2s ease-in-out` 
                     }}
                 >
                     {userData ? (
@@ -143,7 +160,7 @@ const Main = () => {
                             }}
                             color="secondary"
                             variant='outlined'
-                            onClick={handleGoogleLogin} 
+                            onClick={handleLearnMoreClick} 
                         >
                             Learn More
                         </Button>
